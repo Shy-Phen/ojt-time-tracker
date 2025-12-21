f118dc3fc8b443f110540c4c4d0b1c42ae20986a
Phen <rdcoca88@gmail.com>
2025-12-21 20:33:41 +0800

    update server file

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import { DOTENV } from "./lib/dotenv.js";
 import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./lib/DB.js";
@@ -17,24 +16,24 @@ const app = express();
 app.use(clerkMiddleware());
 
 //Middleware
-app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/time", timeRoutes);
 
-app.get("/", (req, res) => {
+app.get("/itawit", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Server is running 🚀",
+    message: "Relax ka lang kaldo ka sangaw🚀",
   });
 });
 
 if (DOTENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Client/dist")));
 
-  app.get(/.*/, (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../Client", "dist", "index.html"));
   });
 }
